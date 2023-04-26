59044c25032ace2676c47c5750267f6733cd2dda
Krzysztof Juszcze <ep_kjuszcze@phreesia.com>
2023-04-26 14:02:33 +0200

    added message broker

---
+++ CatalogService.Infrastructure/MessageBroker/MessageBrokerRegistration.cs
@@ -0,0 +1,15 @@
+﻿using CatalogService.Application;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CatalogService.Infrastructure.MessageBroker;
+
+public static class MessageBrokerRegistration
+{
+    public static void AddMessageBroker(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<RabbitMqOptions>(config.GetSection(RabbitMqOptions.Position));
+        services.AddSingleton<RabbitMqFactory>();
+        services.AddTransient(typeof(IMessageBroker<>), typeof(RabbitMq<>));
+    }
+}
